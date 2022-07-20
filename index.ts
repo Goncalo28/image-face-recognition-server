dab5155ea1870c3b310940f06440d387c48cef64
Goncalo Marques <goncalomarques9@hotmail.com>
2022-07-21 00:12:57 +0100

    pg as client

--- index.ts
+++ index.ts
@@ -9,14 +9,13 @@ const PORT = process.env.PORT || 5000;
 
 const CLARIFAI_API = process.env.CLARIFAI_API;
 const DATABASE_URL = process.env.DATABASE_URL;
-const DB_CLIENT = process.env.DB_CLIENT;
 
 const clarifaiApi: any = new Clarifai.App({
   apiKey: CLARIFAI_API,
 });
 
 const db = knex({
-  client: DB_CLIENT,
+  client: "pg",
   connection: {
     connectionString: DATABASE_URL,
     ssl: {
